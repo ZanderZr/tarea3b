23a67bb170b2851a14043b30321a85d4c1615375
jczr8 <jczr8@LAPTOP-4M5FRU1N>
2023-05-20 13:11:31 +0200

    Utiliza las funciones de refactorización para sustituir el código que se encarga de especificar la calificación en función de la nota por un método que llamarás "cali", que será público, de forma que se incluyan los comentarios para la documentación si ello es posible. Muestra cómo se hace en los dos entornos. (En Eclipse). Sube las modificaciones al repositorio de Github, siendo el comentario el enunciado de este ejercicio.

--- notas5/src/notas/Notas.java
+++ notas5/src/notas/Notas.java
@@ -5,24 +5,31 @@ import java.util.Scanner;
 public class Notas {
 
 public static void main(String[] args) {
-Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
-int notas = 0;
-String c = "";
-System.out.print("Introduzca el valor de una calificación: ");
-notas = reader.nextInt();
+cali();
+}
 
-if (notas >= 0 && notas < 5)
-c = "Suspenso";
-else if (notas >= 5 && notas < 7)
-c = "Aprobado";
-else if (notas >= 7 && notas <9)
-c = "Notable";
-else if (notas >= 9 && notas <= 10)
-c = "Sobresaliente";
-else
-c = "El valor de la calificación introducida no es correcta";
-
-System.out.println(c);
-reader.close();
+/**
+ * 
+ */
+public static void cali() {
+	Scanner reader = new Scanner(System.in);// Para pedir un dato numérico por teclado
+	int notas = 0;
+	String c = "";
+	System.out.print("Introduzca el valor de una calificación: ");
+	notas = reader.nextInt();
+	
+	if (notas >= 0 && notas < 5)
+	c = "Suspenso";
+	else if (notas >= 5 && notas < 7)
+	c = "Aprobado";
+	else if (notas >= 7 && notas <9)
+	c = "Notable";
+	else if (notas >= 9 && notas <= 10)
+	c = "Sobresaliente";
+	else
+	c = "El valor de la calificación introducida no es correcta";
+	
+	System.out.println(c);
+	reader.close();
 }
 }
